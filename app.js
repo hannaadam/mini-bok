32e3cd70d7eb2005939fb4c47bdb564e9fed316c
AdamWendell <Adam@websmith.se>
2016-02-15 21:11:04 +0100

    Added storage folder. Updated app.js.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   res.render(__dirname + '/views/index')
+})
+
+app.post('/skapabok', function (req, res) {
+  console.log(req.body)
 })
 
 app.listen(3000);
